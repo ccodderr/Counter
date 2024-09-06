7d90bbffd865d1ee2aee4d873eff6eb15c60dcd6
ccodderr <crptcharm@gmail.com>
2024-09-06 13:25:46 +0300

    Code refactoring

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,46 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var resetCountButton: UIButton!
+    @IBOutlet private weak var resetCountButton: UIButton!
     
-    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet private weak var increaseButton: UIButton!
     
-    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet private weak var decreaseButton: UIButton!
     
-    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet private weak var countLabel: UILabel!
 
-    @IBOutlet weak var changeHistoryTextView: UITextView!
+    @IBOutlet private weak var historyTextView: UITextView!
     
     private var count: Int = 0 {
         didSet {
-            if count < 0 {
+            guard count > 0 else {
                 count = 0
+                return
             }
         }
     }
     
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        return dateFormatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        increaseButton.layer.cornerRadius = 15
-        decreaseButton.layer.cornerRadius = 15
-        changeHistoryTextView.layer.cornerRadius = 15
-        changeHistoryTextView.isEditable = false
-        changeHistoryTextView.isScrollEnabled = true
+        
+        setupLayout()
     }
     
-    @IBAction func resetAction(_ sender: Any) {
+    private func setupLayout() {
+        increaseButton.layer.cornerRadius = 15
+        decreaseButton.layer.cornerRadius = 15
+        historyTextView.layer.cornerRadius = 15
+        historyTextView.isEditable = false
+        historyTextView.isScrollEnabled = true
+    }
+    
+    @IBAction private func resetAction(_ sender: Any) {
         count = 0
         countLabel.text = "0"
         updateChangeHistory(with: "значение сброшено")
     }
     
-    @IBAction func increaseAction(_ sender: Any) {
+    @IBAction private func increaseAction(_ sender: Any) {
         count += 1
         countLabel.text = "Значение счётчика: \(count)"
         updateChangeHistory(with: "значение изменено на +1")
     }
     
-    @IBAction func decreaseAction(_ sender: Any) {
+    @IBAction private func decreaseAction(_ sender: Any) {
         count -= 1
         guard count > .zero else {
             countLabel.text = "0"
@@ -61,17 +74,14 @@ class ViewController: UIViewController {
 }
 
 extension ViewController {
-    func getCurrentFormattedDate() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .medium
+    private func getCurrentFormattedDate() -> String {
         let currentDate = Date()
         return dateFormatter.string(from: currentDate)
     }
     
-    func updateChangeHistory(with message: String) {
+    private func updateChangeHistory(with message: String) {
         let currentDateTime = getCurrentFormattedDate()
-        changeHistoryTextView.text += "\(currentDateTime): \(message)\n"
+        historyTextView.text += "\(currentDateTime): \(message)\n"
     }
     
 }
